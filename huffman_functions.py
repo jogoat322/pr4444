7d78fe76d005a2adb88f16c46e219d52954f8af4
jogoat322 <zholninaa@gmail.com>
2024-03-11 17:22:05 +0300

    praktika4

---
+++ huffman_functions.py
@@ -0,0 +1,78 @@
+import json
+import heapq
+from collections import defaultdict
+from datetime import datetime
+import os
+import math
+
+class HuffmanCoding:
+    def __init__(self):
+        self.huffman_code = None
+
+    def generate_huffman_code(self, text):
+        frequency = defaultdict(int)
+
+        for char in text:
+            frequency[char] += 1
+
+        heap = [[weight, [symbol, ""]] for symbol, weight in frequency.items()]
+        heapq.heapify(heap)
+
+        while len(heap) > 1:
+            lo = heapq.heappop(heap)
+            hi = heapq.heappop(heap)
+            for pair in lo[1:]:
+                pair[1] = '0' + pair[1]
+            for pair in hi[1:]:
+                pair[1] = '1' + pair[1]
+            heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+
+        self.huffman_code = sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+
+    def save_huffman_code_to_json(self):
+        timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        folder_name = f"{timestamp}"
+        os.makedirs(folder_name, exist_ok=True)
+
+        code_file_path = os.path.join(folder_name, "code.json")
+
+        with open(code_file_path, "w") as code_file:
+            json.dump(dict(self.huffman_code), code_file, ensure_ascii=False, indent=4)
+
+        print(f"Huffman code saved in {code_file_path}")
+
+    def decode_huffman(self, encoded_text):
+        reversed_huffman_code = {code: char for char, code in dict(self.huffman_code).items()}
+        decoded_text = ""
+        current_code = ""
+
+        for bit in encoded_text:
+            current_code += bit
+            if current_code in reversed_huffman_code:
+                decoded_text += reversed_huffman_code[current_code]
+                current_code = ""
+
+        return decoded_text
+
+def calculate_entropy(file_path):
+    with open(file_path, 'rb') as file:
+        byte_freq = {}
+        total_bytes = 0
+        byte = file.read(1)
+        while byte:
+            total_bytes += 1
+            byte_freq[byte] = byte_freq.get(byte, 0) + 1
+            byte = file.read(1)
+
+        entropy = 0
+        for freq in byte_freq.values():
+            probability = freq / total_bytes
+            entropy -= probability * math.log2(probability)
+
+        return entropy
+
+def calculate_file_size(file_path):
+    return os.path.getsize(file_path)
+
+def calculate_compression_ratio(original_size, compressed_size):
+    return original_size / compressed_size
--- huffman_menu.py
+++ huffman_menu.py
@@ -1,98 +1,11 @@
+
+from huffman_functions import *
 import json
-import heapq
-from collections import defaultdict
-from datetime import datetime
-import os
-import math
+huffman_coder = HuffmanCoding()
 
-class HuffmanCoding:
-    def __init__(self):
-        self.huffman_code = None
 
-    def generate_huffman_code(self, text):
-        frequency = defaultdict(int)
 
-        for char in text:
-            frequency[char] += 1
-
-        heap = [[weight, [symbol, ""]] for symbol, weight in frequency.items()]
-        heapq.heapify(heap)
-
-        while len(heap) > 1:
-            lo = heapq.heappop(heap)
-            hi = heapq.heappop(heap)
-            for pair in lo[1:]:
-                pair[1] = '0' + pair[1]
-            for pair in hi[1:]:
-                pair[1] = '1' + pair[1]
-            heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
-
-        self.huffman_code = sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
-
-    def save_huffman_code_to_json(self):
-        timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        folder_name = f"{timestamp}"
-        os.makedirs(folder_name, exist_ok=True)
-
-        code_file_path = os.path.join(folder_name, "code.json")
-
-        with open(code_file_path, "w") as code_file:
-            json.dump(dict(self.huffman_code), code_file, ensure_ascii=False, indent=4)
-
-        print(f"Huffman code saved in {code_file_path}")
-
-    def save_decoded_text_to_file(self, decoded_text):
-        timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        folder_name = f"{timestamp}"
-        os.makedirs(folder_name, exist_ok=True)
-
-        decoded_file_path = os.path.join(folder_name, "decoded_text.txt")
-
-        with open(decoded_file_path, "w", encoding="utf-8") as decoded_file:
-            decoded_file.write(decoded_text)
-
-        print(f"Decoded text saved in {decoded_file_path}")
-
-    def decode_huffman(self, encoded_text):
-        reversed_huffman_code = {code: char for char, code in dict(self.huffman_code).items()}
-        decoded_text = ""
-        current_code = ""
-
-        for bit in encoded_text:
-            current_code += bit
-            if current_code in reversed_huffman_code:
-                decoded_text += reversed_huffman_code[current_code]
-                current_code = ""
-
-        return decoded_text
-
-def calculate_entropy(file_path):
-    with open(file_path, 'rb') as file:
-        byte_freq = {}
-        total_bytes = 0
-        byte = file.read(1)
-        while byte:
-            total_bytes += 1
-            byte_freq[byte] = byte_freq.get(byte, 0) + 1
-            byte = file.read(1)
-
-        entropy = 0
-        for freq in byte_freq.values():
-            probability = freq / total_bytes
-            entropy -= probability * math.log2(probability)
-
-        return entropy
-
-def calculate_file_size(file_path):
-    return os.path.getsize(file_path)
-
-def calculate_compression_ratio(original_size, compressed_size):
-    return original_size / compressed_size
-
-def main():
-    huffman_coder = HuffmanCoding()
-
-    while True:
+while True:
         print("1. Кодирование Huffman")
         print("2. Декодирование Huffman")
         print("3. Вывод информации о файлах")
@@ -131,10 +44,6 @@ def main():
             decoded_text = huffman_coder.decode_huffman(encoded_text)
             print(f"Декодированный текст: {decoded_text}")
 
-            save_option = input("Сохранить декодированный текст в файл? (y/n): ")
-            if save_option.lower() == "y":
-                huffman_coder.save_decoded_text_to_file(decoded_text)
-
         elif choice == "3":
             input_file_path = input("Введите путь к исходному файлу: ")
             compressed_file_path = input("Введите путь к закодированному файлу: ")
@@ -166,5 +75,4 @@ def main():
         else:
             print("Неверный выбор. Выберите 1, 2, 3 или 4.")
 
-if __name__ == "__main__":
-    main()
+
